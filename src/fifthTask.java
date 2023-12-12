dbd03860939dec63e025d60117e4b1e25e0c2ae0
Rustam Shamsutdinov <sypooit@gmail.com>
2023-12-12 11:05:28 +0300

    fix 5th tasks

--- src/fifthTask.java
+++ src/fifthTask.java
@@ -20,6 +20,9 @@ public class fifthTask {
         System.out.println(spiderVsFly("H3", "E2"));
         System.out.println(spiderVsFly("A4", "B2"));
         System.out.println(spiderVsFly("A4", "C2"));
+        System.out.println(spiderVsFly("A4" ,"H2"));
+        System.out.println(spiderVsFly("A2", "H4"));
+        System.out.println(spiderVsFly("G2", "A2"));
         System.out.println("3) digitsCount");
         System.out.println(digitsCount(4666));
         System.out.println(digitsCount(544));
@@ -58,7 +61,7 @@ public class fifthTask {
         System.out.println(isNew(3));
         System.out.println(isNew(30));
         System.out.println(isNew(321));
-        System.out.println(isNew(123));
+        System.out.println(isNew(130));
     }
 
     public static boolean sameLetterPattern(String s1, String s2) {
@@ -77,62 +80,84 @@ public class fifthTask {
 
     public static String spiderVsFly(String spiderPosition, String flyPosition) {
         System.out.println("From "+ spiderPosition + " to " + flyPosition + ":");
-        int startLetter = spiderPosition.charAt(0) - 'A' + 1;
-        int startNumber = spiderPosition.charAt(1) - '0';
-        int endLetter = flyPosition.charAt(0) - 'A' + 1;
-        int endNumber = flyPosition.charAt(1) - '0';
 
-        List<String> path = new ArrayList<>();
-        while (startNumber > endNumber) {
-            path.add("" + (char) (startLetter + 'A' - 1) + startNumber);
-            startNumber--;
+        int spiderSymbol = spiderPosition.charAt(0) - 65;
+        int spiderNumber = Integer.parseInt(spiderPosition.substring(1, 2));
+        int flySymbol = flyPosition.charAt(0) - 65;
+        int flyNumber = Integer.parseInt(flyPosition.substring(1, 2));
+        if (flyNumber == 0) flySymbol = 0;
+
+
+        double straightWay = 1;
+        double[] ways = new double[5];
+        for (int i = 0; i < 5; i++) ways[i] = i * Math.sqrt(2 - Math.sqrt(2));
+
+        StringBuilder strWay1 = new StringBuilder(spiderPosition);
+        double intWay1 = 0;
+        int nowSpiderSymbol1 = spiderSymbol;
+        int nowSpiderNumber1 = spiderNumber;
+        while (nowSpiderSymbol1 != flySymbol || nowSpiderNumber1 != flyNumber) {
+            if (nowSpiderNumber1 == 0) {
+                nowSpiderSymbol1 = flySymbol;
+                nowSpiderNumber1 = 1;
+            } else if (nowSpiderSymbol1 == spiderSymbol) {
+                nowSpiderNumber1 -= 1;
+                if (nowSpiderNumber1 == 0) nowSpiderSymbol1 = 0;
+            } else {
+                nowSpiderNumber1 += 1;
+            }
+            strWay1.append("-").append((char) (nowSpiderSymbol1 + 65)).append(nowSpiderNumber1);
+            intWay1 += straightWay;
         }
-        List<String> pathEnd = new ArrayList<>();
-        while (startNumber < endNumber) {
-            pathEnd.add("" + (char) (endLetter + 'A' - 1) + endNumber);
-            endNumber--;
-        }
-        int distance = Math.abs(startLetter - endLetter); // разница в буквах
-        if (distance > 4)
-            distance = 8 - distance;
-        if (distance <= 2) { // по дуге
-            path.add("" + (char) (startLetter + 'A' - 1) + startNumber);
-            while (startLetter != endLetter) {
-                if (Math.abs(startLetter - endLetter) > 4) {
-                    startLetter--;
-                    if (startLetter == 0)
-                        startLetter = 8;
-                } else {
-                    startLetter++;
-                    if (startLetter == 9)
-                        startLetter = 1;
+
+        StringBuilder strWay2 = new StringBuilder(spiderPosition);
+        double intWay2 = 0;
+        int nowSpiderSymbol2 = spiderSymbol;
+        int nowSpiderNumber2 = spiderNumber;
+        if (nowSpiderNumber2 > flyNumber) {
+            while (nowSpiderNumber2 != flyNumber) {
+                nowSpiderNumber2 -= 1;
+                strWay2.append("-").append((char) (nowSpiderSymbol2 + 65)).append(nowSpiderNumber2);
+                intWay2 += straightWay;
+            }
+            while (nowSpiderSymbol2 != flySymbol) {
+                int clock = flySymbol - nowSpiderSymbol2;
+                if (clock < 0) clock += 8;
+                if (clock <= 4) {
+                    nowSpiderSymbol2 = (nowSpiderSymbol2 + 1) % 8;
+
                 }
-                path.add("" + (char) (startLetter + 'A' - 1) + startNumber);
-            }
-        } else { // через центр
-            while (startNumber != 0) {
-                path.add("" + (char) (startLetter + 'A' - 1) + startNumber);
-                startNumber--;
-            }
-            path.add("" + (char) ('A') + startNumber);
-            while (startNumber != endNumber) {
-                startNumber++;
-                path.add("" + (char) (endLetter + 'A' - 1) + startNumber);
+                else {
+                    nowSpiderSymbol2 = (nowSpiderSymbol2 + 7) % 8;
+                }
+                strWay2.append("-").append((char) (nowSpiderSymbol2 + 65)).append(nowSpiderNumber2);
+                intWay2 += ways[nowSpiderNumber2];
             }
         }
+        else {
+            while (nowSpiderSymbol2 != flySymbol) {
+                int clock = flySymbol - nowSpiderSymbol2;
+                if (clock < 0) clock += 8;
+                if (clock <= 4) {
+                    nowSpiderSymbol2 = (nowSpiderSymbol2 + 1) % 8;
 
-        // Собираем путь в строку
-        StringBuilder result = new StringBuilder();
-        for (String step : path) {
-            result.append(step);
-            result.append("-");
+                }
+                else {
+                    nowSpiderSymbol2 = (nowSpiderSymbol2 + 7) % 8;
+                }
+                strWay2.append("-").append((char) (nowSpiderSymbol2 + 65)).append(nowSpiderNumber2);
+                intWay2 += ways[nowSpiderNumber2];
+            }
+            while (nowSpiderNumber2 != flyNumber) {
+                nowSpiderNumber2 += 1;
+                strWay2.append("-").append((char) (nowSpiderSymbol2 + 65)).append(nowSpiderNumber2);
+                intWay2 += straightWay;
+            }
         }
-        result.deleteCharAt(result.length() - 1);
-        for (int i = pathEnd.size() - 1; i >= 0; i--) {
-            result.append("-");
-            result.append(pathEnd.get(i));
+        if (intWay1 <= intWay2) {
+            return strWay1.toString();
         }
-        return result.toString();
+        return strWay2.toString();
     }
 
 
@@ -178,12 +203,12 @@ public class fifthTask {
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] + a[j] == 8) {
-                    ans.add(new int[] { Math.min(a[i], a[j]), Math.max(a[i], a[j]) });
+                    ans.add(new int[] { Math.min(a[i], a[j]), Math.max(a[i], a[j]), j - i });
                 }
             }
         }
-        ans.sort(Comparator.comparing((int[] x) -> x[0]));
-        return ans.toArray(new int[ans.size()][2]);
+        ans.sort(Comparator.comparing((int[] x) -> x[2]));
+        return ans.stream().map(arr -> Arrays.copyOf(arr, 2)).toArray(int[][]::new);
     }
 
     // 6
